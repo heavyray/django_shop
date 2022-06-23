40127a05a31f381bf5e8e9fbe93c5e00669f50b2
Maksim Polivnikov <heavy97@yandex.ru>
2022-06-23 17:20:25 +0300

    Some changes

--- myshop/settings.py
+++ myshop/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,8 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jzrjwy8*erz!-jcuf_j4+cuu481w+d544if#ebik62h+1id6h6'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
